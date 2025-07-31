ce0677abd6017a0dd85f90ce557e9185e15fde8b
Yukti <yuktimarwahgithub@gmail.com>
2025-07-31 13:46:49 +0530

    add lec_01

---
+++ lec_01_variable/variable.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // integer
+    cout<<".......integer......."<<endl;
+    int pincode =175001;
+    int number=89;
+    int age=22;
+
+    cout<<"pincode ="<<pincode<<endl;
+    cout<<"number="<<number<<endl;
+    cout<<"age="<<age<<endl;
+
+    cout<<".......float......."<<endl;
+    float bank_balance = 986907.68;
+    float salary = 976.76;
+
+    cout<<"bank_balance="<<bank_balance<<endl;
+    cout<<"salary="<<salary<<endl;
+
+    cout<<".......char......."<<endl;
+    char symbol_1 ='1';
+    char symbol_2 = 'a';
+    char symbol_3 = '@';
+
+    cout<<"symbol 1="<<symbol_1<<endl<<"symbol 2="<<symbol_2<<endl<<"symbol 3="<<symbol_3<<endl;
+
+    cout<<".......string......."<<endl;
+    string name = "spider-man";
+    string str = "fnir4tj3";
+    string str2 = "abc123";
+
+    cout<<"name ="<<name<<endl<<"str="<<str<<endl<<"str2="<<str2;
+
+
+    return 0;
+}
